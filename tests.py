352d5e15ed1142981724879828a89d80e097a63f
ajaypallekonda <32649063+ajaypallekonda@users.noreply.github.com>
2020-03-01 21:10:19 -0800

    updated function2 part 3

--- tests.py
+++ tests.py
@@ -19,7 +19,7 @@ class TestCase(unittest.TestCase):
 
     def test_function2(self):
         list = [1, 2, 3, 4, 5, 6, 7, 8]
-        self.assertEqual((1,8), task.function2(list))
+        self.assertEqual((1, 8), task.function2(list))
 
 
 if __name__ == "__main__":
